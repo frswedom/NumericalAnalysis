00e60405a6a23b8b1a0943d1c69d2f782805cc0a
Yehor Bondar <frswedom@gmail.com>
2016-10-18 20:56:37 +0300

    First commit
    Added: Kahan summation algorithm

---
+++ kahan_summation.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    
+}
+
+double kahan_sum(vector<double>& seq) {
+    double sum = 0.0, c = 0.0;
+    double y, t;
+    for(double k : seq) {
+        y = k - c;
+        t = sum + y;
+        c = (t - sum) - y;
+        sum = t;
+    }
+    return sum;
+}
